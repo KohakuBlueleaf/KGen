8994175b9c6e158a2dd961b96abeb464fd236df9
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-04-23 16:06:12 +0800

    0.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="tipo-kgen",
     packages=find_packages(),
-    version="0.0.7",
+    version="0.0.9",
     license="Apache 2.0",
     url="https://github.com/KohakuBlueleaf/KGen",
     description=(
