ec8afacccd339201826e92e5b75da01c9a6605c2
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-11-18 23:24:18 +0800

    Update model_speed.py

--- scripts/benchmark/model_speed.py
+++ scripts/benchmark/model_speed.py
@@ -26,27 +26,10 @@ DEFAULT_FORMAT = """<|special|>, <|characters|>, <|copyrights|>,
 <|quality|>, <|meta|>, <|rating|>
 """
 tipo.BAN_TAGS = []
-tipo.retry_criteria = lambda *args: True
 
 
 logging.set_verbosity_error()
 print(f"threads: {torch.get_num_threads()} {torch.get_num_interop_threads()}")
-
-model_name, gguf_list = models.tipo_model_list[-2]
-models.download_gguf(model_name, gguf_list[0])
-models.load_model(
-    os.path.basename(model_name)+"_"+gguf_list[0],
-    gguf=True,
-    device="cuda",
-    # main_gpu=0,
-)
-# models.load_model(
-#     model_name,
-#     gguf=False,
-#     device="cuda",
-#     # main_gpu=0,
-# )
-generate(max_new_tokens=4)
 
 
 def task(tags: list[str], nl_prompt: str):
@@ -60,59 +43,74 @@ def task(tags: list[str], nl_prompt: str):
         and "<|generated|>" in DEFAULT_FORMAT
         or (
             not nl_prompt
-            and (
-                "<|generated|>" in DEFAULT_FORMAT 
-                or "<|extended|>" in DEFAULT_FORMAT
-            )
+            and ("<|generated|>" in DEFAULT_FORMAT or "<|extended|>" in DEFAULT_FORMAT)
         ),
     )
-    print(operations)
     meta["aspect_ratio"] = f"{width / height:.1f}"
-    result, timing = tipo.tipo_runner(meta, operations, general, nl_prompt)
+    result, timing = tipo.tipo_runner(
+        meta, operations, general, nl_prompt, retry_criteria=lambda *args: True
+    )
     formatted = re.sub(r"([()\[\]])", r"\\\1", apply_format(result, DEFAULT_FORMAT))
     return formatted, timing
 
 
 if __name__ == "__main__":
-    test = 1#00
+    test = 100
 
-    with open("./data/danbooru.json", "rb") as f:
-        data = loads(f.read())
-    entries = []
-    for entry in tqdm(data[:test], smoothing=0.01):
-        short_caption = remove_repeated_suffix(entry["florence_short"])
-        long_caption = remove_repeated_suffix(
-            entry.get("phi3v_horny", None) or entry["florence_long"]
+    for model_name, gguf_list in models.tipo_model_list:
+        print("Using model:", model_name)
+        models.download_gguf(model_name, gguf_list[0])
+        # models.load_model(
+        #     os.path.basename(model_name) + "_" + gguf_list[0],
+        #     gguf=True,
+        #     device="cuda",
+        #     # main_gpu=0,
+        # )
+        models.load_model(
+            model_name,
+            gguf=False,
+            device="cuda",
+            # main_gpu=0,
         )
-        # Some outlier have TOO MANY tags, need to cutoff
-        info_tags = (
-            entry["artist"][:5]
-            + entry["character"][:5]
-            + entry["copyright"][:5]
-            + entry["meta"][:10]
-            + entry["rating"]
-            + entry["special"]
-            + entry["year"]
-        )
-        content_tags = entry["general"]
-        random.shuffle(content_tags)
-        tags = info_tags + content_tags[:5]
-        entries.append((tags, short_caption, long_caption))
+        generate(max_new_tokens=4)
 
-    results = []
-    for i in trange(test):
-        tags, short_caption, long_caption = entries[i]
-        t0 = time()
-        formatted, timing = task(tags, "")
-        t1 = time()
-        timing["generate_time"] = t1 - t0
-        results.append(timing)
+        with open("./data/danbooru.json", "rb") as f:
+            data = loads(f.read())
+        entries = []
+        for entry in tqdm(data[:test], smoothing=0.01):
+            short_caption = remove_repeated_suffix(entry["florence_short"])
+            long_caption = remove_repeated_suffix(
+                entry.get("phi3v_horny", None) or entry["florence_long"]
+            )
+            # Some outlier have TOO MANY tags, need to cutoff
+            info_tags = (
+                entry["artist"][:5]
+                + entry["character"][:5]
+                + entry["copyright"][:5]
+                + entry["meta"][:10]
+                + entry["rating"]
+                + entry["special"]
+                + entry["year"]
+            )
+            content_tags = entry["general"]
+            random.shuffle(content_tags)
+            tags = info_tags + content_tags[:5]
+            entries.append((tags, short_caption, long_caption))
 
-    total = {}
-    for k in results[0].keys():
-        total[k] = sum([x[k] for x in results])
+        results = []
+        for i in trange(test):
+            tags, short_caption, long_caption = entries[i]
+            t0 = time()
+            formatted, timing = task(tags, "")
+            t1 = time()
+            timing["generate_time"] = t1 - t0
+            results.append(timing)
 
-    average = {k: v / test for k, v in total.items()}
+        total = {}
+        for k in results[0].keys():
+            total[k] = sum([x[k] for x in results])
 
-    objprint(total)
-    objprint(average)
+        average = {k: v / test for k, v in total.items()}
+
+        objprint(total)
+        objprint(average)
